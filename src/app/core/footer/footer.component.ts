567be1d165a8963d9f2a9081de39e82ef1c58e16
phuongtrannam <trannamphuonga197@gmail.com>
2020-03-02 15:24:08 +0700

    before install angular-count-to library

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    $('.elementor-counter-number').each(function () {
+      $(this).prop('Counter', 0).animate({
+              Counter: $(this).data('value')
+          }, {
+          duration: 1000,
+          easing: 'swing',
+          step: function (now) {                      
+              $(this).text(this.Counter.toFixed(2));
+          }
+      });
+  });
+  }
 
   ngOnInit() {
+
   }
 
 }
