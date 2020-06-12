c62544ea63d24f3e5c115e61d6cb18a0f8b1fe1a
John Doe <einsteintenth@gmail.com>
2020-06-12 17:00:33 +0700

    fix_seach

--- src/app/modules/home/components/home-search/home-search.component.ts
+++ src/app/modules/home/components/home-search/home-search.component.ts
@@ -4,12 +4,27 @@ import { HomeService } from '../../home.service';
 @Component({
   selector: 'app-home-search',
   templateUrl: './home-search.component.html',
-  styleUrls: ['./home-search.component.css'],
+  styleUrls: ['./home-search.component.scss'],
   providers: [HomeService]
 })
 export class HomeSearchComponent implements OnInit {
-
-
+  public menu: any = [
+    {name: 'Lĩnh vực', selected: true, placehoder: 'VD: CNTT', logo: 'fa fa-home'},
+    {name: 'Ngành nghề', selected: false, placehoder: 'VD: Lập trình Web', logo: 'fa fa-seach'},
+    {name: 'Công việc', selected: false, placehoder: 'VD: FullStack DEV', logo: 'fa fa-dragon '},
+    
+  ];
+  logo= "fa fa-home";
+  ph= 'VD: CNTT';
+  
+  thayDoi(index) {
+    this.menu.forEach(element => {
+      element.selected = false;
+    });
+    this.menu[index].selected = true;
+    this.logo=this.menu[index].logo;
+    this.ph= this.menu[index].placehoder;
+  }
   constructor(private homeService: HomeService) {
 
   }
