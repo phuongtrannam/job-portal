41f0b4cd4af1c0beec611f5db8422c1953e37747
phuongtrannam <trannamphuonga197@gmail.com>
2020-06-28 17:09:06 +0700

    add loading component

---
+++ src/app/core/loading/loading.component.ts
@@ -0,0 +1,14 @@
+
+import { Component, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'app-loading',
+    template: `		<div id="pause" class="d-flex align-items-center justify-content-center">
+									<div id="spinner"></div>
+								</div>`,
+    styleUrls: ['loading.component.scss']
+})
+
+export class LoadingComponent {
+
+}
